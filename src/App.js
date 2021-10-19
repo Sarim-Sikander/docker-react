08e74dc9c08637cddd1b6738c4dc5dc6912a2f3e
Sarim-Sikander <Sarimsikander24@gmail.com>
2021-10-19 22:54:20 +0500

    Added final choice

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          How are you there?
+          How are you there
         </p>
         <a
           className="App-link"
